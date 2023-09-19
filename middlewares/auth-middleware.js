5a1454da6eda80f3adab892046aa993c020dd954
Savinoff94 <andreisavinovwork@gmail.com>
2023-09-19 22:45:48 +0300

    middleware fix

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -4,17 +4,20 @@ const tokenService = require('../service/token-service')
 module.exports = function(req,res,next) {
     
     try {
-        const authorizationHeader = req.headers.Authorization
+        const authorizationHeader = req.headers.authorization
+       
         if(!authorizationHeader) {
             return next(ApiError.UnauthorizedError())
         }
 
         const accessToken = authorizationHeader.split(' ')[1]
+        
         if(!accessToken) {
             return next(ApiError.UnauthorizedError())
         }
 
         const userData = tokenService.validateAccessToken(accessToken)
+        
         if(!userData) {
             return next(ApiError.UnauthorizedError())
         }
