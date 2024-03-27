46eaf593166c021c5ee9c3de5895332535be86ac
Savinoff94 <andreisavinovwork@gmail.com>
2024-03-27 20:51:27 +0200

    connect to postgres on railway

--- modules/db.js
+++ modules/db.js
@@ -4,10 +4,20 @@ module.exports = {
   db: knex({
     client:'pg',
     connection:{
-      host:'127.0.0.1',
-      user:'postgres',
-      password:process.env.POSTGRESS_PASSWORD,
-      database:'postgres'
+      connectionString:process.env.DB_POSTGRES_PROD_URL,
     }
   })
 }
+
+// Local
+// module.exports = {
+//   db: knex({
+//     client:'pg',
+//     connection:{
+//       host:'127.0.0.1',
+//       user:'postgres',
+//       password:process.env.POSTGRESS_PASSWORD,
+//       database:'postgres'
+//     }
+//   })
+// }
